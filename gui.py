f681974bd57a5d256e17a9f2918a2a2eb8c8fd2f
Vincent Verdeil <werdeil@hotmail.com>
2015-06-17 11:15:24 +0200

    Improvements of the interface
    - The duration between each draw is now defined in the gui, and handled by the interface
    - The menu have been renamed
    - Pressing cancel when opening a list is now taken into account
    - Addition of a progress bar to show when the next draw will be done
    - Small PEP8 correction

--- gui.py
+++ gui.py
@@ -4,13 +4,15 @@
 __author__ = 'werdeil'
 
 from Tkinter import *
+from ttk import Progressbar
 import tkFileDialog
+from tkMessageBox import showinfo, showerror
 import tombola
 import time
 
 
 class InterfaceGauche(LabelFrame):
-    '''Left frame of the GUI, giving the buttons and the current price and winner'''
+    """Left frame of the GUI, giving the buttons and the current price and winner"""
     def __init__(self, tk_frame, waiting_time, **kwargs):
         LabelFrame.__init__(self, tk_frame, text="Tirage", **kwargs)
         self.waiting_time = waiting_time
@@ -39,33 +41,46 @@ class InterfaceGauche(LabelFrame):
         self.parent = self._nametowidget(self.parent_name)
 
         # partie sablier
-        self.next_draw = Label(self, text="Prochain tirage dans")
-        self.next_draw.grid(column=0, row=6, columnspan=1)
-        self.next_wait = Label(self, text="%s s" % self.waiting_time)
-        self.next_wait.grid(column=1, row=6, columnspan=1)
+        self.interval = Label(self, text="Intervalle entre tirages (s)")
+        self.interval.grid(column=0, row=6, columnspan=1)
+        self.v = StringVar()
+        self.v.set(self.waiting_time)
+        self.interval_length = Entry(self, textvariable=self.v,  width=5, justify=CENTER)
+        self.interval_length.grid(column=1, row=6, columnspan=1)
+        self.progress_bar = Progressbar(self, length=300)
+        self.progress_bar.grid(column=0, row=8, columnspan=2, pady=10)
         self.nb_players_text = Label(self, text="Nombre de joueurs")
-        self.nb_players_text.grid(column=0, row=8, columnspan=1)
+        self.nb_players_text.grid(column=0, row=10, columnspan=1)
         self.nb_players = Label(self, text="0")
-        self.nb_players.grid(column=1, row=8, columnspan=1)
+        self.nb_players.grid(column=1, row=10, columnspan=1)
         self.nb_prices_text = Label(self, text="Nombre de prix restants")
-        self.nb_prices_text.grid(column=0, row=9, columnspan=1)
+        self.nb_prices_text.grid(column=0, row=11, columnspan=1)
         self.nb_prices = Label(self, text="0")
-        self.nb_prices.grid(column=1, row=9, columnspan=1)
+        self.nb_prices.grid(column=1, row=11, columnspan=1)
 
         # for i in range(5):
         self.grid_rowconfigure(5, weight=1)
-        self.grid_rowconfigure(6, weight=1)
+        # self.grid_rowconfigure(6, weight=1)
+        # self.grid_rowconfigure(7, weight=1)
+        self.grid_rowconfigure(9, weight=1)
         for i in range(2):
             self.grid_columnconfigure(i, weight=1)
 
     def click(self):
-        '''When click is selected, the tombola starts'''
-       
+        """When click is selected, the tombola starts"""
+        try:
+            self.waiting_time = int(self.v.get())
+        except ValueError:
+            showerror("Error", "L'intervalle doit être un nombre")
+            return
+        self.progress_bar['maximum'] = self.waiting_time+0.01
+        self.interval_length.config(state=DISABLED)
         self.parent.draw_tombola(self.waiting_time)
 
         if not self.parent.list_prices:
             self.bouton_cliquer.config(state=DISABLED)
-            self.next_wait["text"] = "Tous les lots ont été tirés"
+            self.price["text"] = "Tous les lots ont été tirés"
+            self.name["text"] = ""
             self.update()
             
     def update_nb_players(self):        
@@ -78,7 +93,7 @@ class InterfaceGauche(LabelFrame):
 
 
 class TableResults(LabelFrame):
-    '''Right frame of the GUI, giving the results of the already won prices'''
+    """Right frame of the GUI, giving the results of the already won prices"""
     def __init__(self, tk_frame, **kwargs):
         LabelFrame.__init__(self, tk_frame, text="Resultats", **kwargs)
         self.names_title = Label(self, text="Nom")
@@ -95,14 +110,14 @@ class TableResults(LabelFrame):
 
 
 class FenetreTombola(Tk):
-    '''Main window containing the 2 frames'''
-    def __init__(self, waiting_time, **kwargs):
+    """Main window containing the 2 frames"""
+    def __init__(self, **kwargs):
         Tk.__init__(self, **kwargs)
         # self.geometry("810x520")
         self.list_names = []
         self.list_prices = []
         self.title("Tombola")
-        self.interface = InterfaceGauche(self, waiting_time)
+        self.interface = InterfaceGauche(self, 10)
         self.interface.grid(column=0, row=0)
         self.results = TableResults(self)
         self.results.grid(column=1, row=0)
@@ -113,44 +128,34 @@ class FenetreTombola(Tk):
         self.menubar = Menu(self)
 
         self.menu1 = Menu(self.menubar, tearoff=0)
-        self.menu1.add_command(label="Importer prix", command=self.load_prices)
         self.menu1.add_command(label="Importer joueurs", command=self.load_names)
+        self.menu1.add_command(label="Importer prix", command=self.load_prices)
         self.menu1.add_separator()
         self.menu1.add_command(label="Quitter", command=self.quit)
-        self.menubar.add_cascade(label="Fichier", menu=self.menu1)
+        self.menubar.add_cascade(label="Menu", menu=self.menu1)
 
         self.menu2 = Menu(self.menubar, tearoff=0)
         self.menu2.add_command(label="A propos", command=self.alert)
         self.menubar.add_cascade(label="Aide", menu=self.menu2)
 
         self.config(menu=self.menubar)
-        
-        # define options for opening or saving a file
-        self.file_opt = options = {}
-        options['defaultextension'] = '.csv'
-        options['filetypes'] = [('all files', '.*'), ('csv files', '.csv')]
-        options['initialdir'] = 'C:\\'
-        options['initialfile'] = 'myfile.csv'
-        options['parent'] = self
-        options['title'] = 'This is a title'
 
     def draw_tombola(self, wait_time):
         while len(self.list_prices) > 0:
             price = self.list_prices.pop(0)
             self.interface.price["text"] = price
             self.interface.name["text"] = ''
-            countdown = wait_time
-            for i in range(countdown+1):
-                self.interface.next_wait["text"] = "%s s" % countdown
-                self.interface.next_wait.update()
+            for i in range(wait_time):
+                self.interface.progress_bar.step()
+                self.interface.progress_bar.update()
                 time.sleep(1)
-                countdown -= 1
             self.update()
             time.sleep(0.25)
             name, self.list_names = tombola.draw_name(self.list_names)
             self.interface.name["text"] = name
+            self.interface.progress_bar['value'] = 0
             self.interface.name.update()
-            time.sleep(1)
+            time.sleep(0.25)
 
             self.results.names["text"] += "%s\n" % name
             self.results.prices["text"] += "%s\n" % price
@@ -158,26 +163,28 @@ class FenetreTombola(Tk):
             self.interface.update_nb_prices()
             self.results.update()
             tombola.write_results('save.csv', name, price)
-            time.sleep(1)
+            time.sleep(0.25)
 
     def alert(self):
-        from tkMessageBox import *
-        showinfo("alerte", "Bravo!")
+        showinfo("Help", "I can't help you", parent=self)
         
     def load_names(self):
         """Returns an opened file in read mode."""
-        self.list_names = tombola.import_lists(tkFileDialog.askopenfilename(**self.file_opt))
-        self.interface.update_nb_players()
+        filename = tkFileDialog.askopenfilename(filetypes=[('all files', '.*'), ('csv files', '.csv')],
+                                                title='Importer la liste des joueurs', parent=self)
+        if len(filename) != 0:
+            self.list_names = tombola.import_lists(filename)
+            self.interface.update_nb_players()
         
     def load_prices(self):
         """Returns an opened file in read mode."""
-        self.list_prices = tombola.import_prices(tkFileDialog.askopenfilename(**self.file_opt))
-        self.interface.update_nb_prices()
+        filename = tkFileDialog.askopenfilename(filetypes=[('all files', '.*'), ('csv files', '.csv')],
+                                                title='Importer la liste des prix', parent=self)
+        if len(filename) != 0:
+            self.list_prices = tombola.import_prices(filename)
+            self.interface.update_nb_prices()
         
         
 if __name__ == "__main__":
-    # list_names = tombola.import_lists("list_names_test.csv")
-    # list_prices = ['pen', 'bag', 'wallet'] # To be corrected to import it from a csv too
-    frame = FenetreTombola(10) # The waiting time shall be parametrisable in the gui
-
+    frame = FenetreTombola()
     frame.mainloop()
