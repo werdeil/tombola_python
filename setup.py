ab791ae6f9b7305829acb96fc465801292f87a10
werdeil <werdeil@hotmail.com>
2015-07-07 21:54:25 +0200

    Move files to package the version 0.1

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(name='tombola_python',
+      version='0.1',
+      description='Small Python interface with Tkinter to draw prices for a tombola',
+      author='Vincent Verdeil',
+      author_email='vincent.verdeil@gmail.com',
+      url='https://github.com/werdeil/tombola_python',
+      scripts=['bin/tombola_python'],
+      packages=['tombola_python'],
+      )
